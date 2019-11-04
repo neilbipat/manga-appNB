0af78b838fff803e5396eabac6f093dcc932033e
Neil BipatE <neilbipat@gmail.com>
2019-11-04 18:19:12 -0500

    made the repository model and created the UserRepository

---
+++ login/src/main/java/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package repository;
+
+import com.example.login.model.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    @Query("FROM User u WHERE u.username = ?1 AND u.password =?2")
+
+    public User login(String username, String password);
+    public User  findByUsername(String username);
+}
