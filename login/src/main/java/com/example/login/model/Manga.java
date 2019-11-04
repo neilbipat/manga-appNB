59d74dc284554c4bd23ad5060e2af340778b84bd
Neil BipatE <neilbipat@gmail.com>
2019-11-04 15:38:48 -0500

    Made the user model and begun to edit the manga model

--- login/src/main/java/com/example/login/model/Manga.java
+++ login/src/main/java/com/example/login/model/Manga.java
@@ -1,4 +1,21 @@
 package com.example.login.model;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "manga")
 public class Manga {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+
 }
--- login/src/main/java/com/example/login/model/User.java
+++ login/src/main/java/com/example/login/model/User.java
@@ -28,7 +28,73 @@ public class User {
     @JoinColumn(name = "manga_id")
     @JsonBackReference
     private Manga manga;
-    
+
+
+    public User() {}
+
+    /**
+     *
+     * @return manga
+     */
+
+    public Manga getManga() {return manga;}
+
+    /**
+     *
+     * @return id
+     */
+
+    public Long getId() {return id;}
+
+    /**
+     *
+     * @param id
+     */
+
+    public void setId(Long id) {this.id = id;}
+
+    /**
+     *
+     * @return username
+     */
+
+    public String getUsername() {return username;}
+
+    /**
+     *
+     * @param username
+     */
+
+    public void setUsername(String username) {this.username = username;}
+
+    /**
+     *
+     * @return email
+     */
+
+    public String getEmail() {return email;}
+
+    /**
+     *
+     * @param email
+     */
+
+    public void setEmail(String email) {this.email = email;}
+
+    /**
+     *
+     * @return password
+     */
+
+    public String getPassword() {return password;}
+
+    /**
+     *
+     * @param password
+     */
+
+    public void setPassword(String password) {this.password = password;}
+
 
 
 }
