8bb7d39b596e479f05cfca705e9f98ceeadbc869
Neil BipatE <neilbipat@gmail.com>
2019-11-04 15:21:06 -0500

    Started the user model with parameters id user email password

--- login/src/main/java/com/example/login/model/User.java
+++ login/src/main/java/com/example/login/model/User.java
@@ -1,6 +1,28 @@
 package com.example.login.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
+import javax.persistence.*;
+import java.util.List;
 
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    
 }
