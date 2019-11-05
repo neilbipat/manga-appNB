f52cf56906405ec91974d1fa23af31ed2a27df3f
Neil BipatE <neilbipat@gmail.com>
2019-11-05 11:49:57 -0500

    Started making the manga controller

---
+++ login/src/main/java/com/example/login/controller/MangaController.java
@@ -0,0 +1,12 @@
+package com.example.login.controller;
+
+import com.example.login.model.Manga;
+import com.example.login.repository.UserRepository;
+import com.example.login.service.MangaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+public class MangaController {
+}
