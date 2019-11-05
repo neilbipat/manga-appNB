1a3d9108416ca2ce45d81645f6cb9f7f86ac06bb
Neil BipatE <neilbipat@gmail.com>
2019-11-05 11:35:30 -0500

    Added the repository with Mangatitle and MangaChapter

--- login/src/main/java/com/example/login/repository/MangaRepository.java
+++ login/src/main/java/com/example/login/repository/MangaRepository.java
@@ -7,4 +7,14 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface MangaRepository extends CrudRepository<Manga, Long> {
+
+    /**
+     *
+     * @param user
+     * @return
+     */
+
+    public List<Manga> findMangasByManga_title(User user);
+
+    public List<Manga> findMangaByManga_chapter(User user;)
 }
