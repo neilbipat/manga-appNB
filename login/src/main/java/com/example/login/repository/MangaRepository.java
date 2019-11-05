03ffd0802f4fcc8c8de334eeddb190a9039d253e
Neil BipatE <neilbipat@gmail.com>
2019-11-05 09:50:46 -0500

    Added a manga repository complete with findByManga_title

---
+++ login/src/main/java/com/example/login/repository/MangaRepository.java
@@ -0,0 +1,21 @@
+package com.example.login.repository;
+
+import com.example.login.model.Manga;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MangaRepository extends CrudRepository<Manga, Long> {
+
+    /**
+     * 
+     * @param manga_title
+     * @return
+     */
+
+    @Query("From Manga m Where m.manga_title = ?1 AND m.manga_chapter =?2")
+
+    public Manga findByManga_title(String manga_title);
+
+}
