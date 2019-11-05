583bdc36c5d862d30f7e22eb47f6c221808de839
Neil BipatE <neilbipat@gmail.com>
2019-11-04 20:06:00 -0500

    Made the iAuthenticationFacade interface to deal with tokens

---
+++ login/src/main/java/com/example/login/config/IAuthenticationFacade.java
@@ -0,0 +1,9 @@
+package com.example.login.config;
+
+import org.springframework.security.core.Authentication;
+
+public interface IAuthenticationFacade {
+
+    Authentication getAuthentication();
+    
+}
