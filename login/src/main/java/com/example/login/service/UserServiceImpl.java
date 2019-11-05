e2d0c2732b74cbe6e749a99bde601cd9ff194bf6
Neil BipatE <neilbipat@gmail.com>
2019-11-04 19:16:17 -0500

    login user

--- login/src/main/java/com/example/login/service/UserServiceImpl.java
+++ login/src/main/java/com/example/login/service/UserServiceImpl.java
@@ -40,6 +40,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getUser(String username) {return userRepository.findByUsername(username); }
 
+    //===parameter username & return===//
     private List<GrantedAuthority> getGrantedAuthorities(User user) {
         List<GrantedAuthority> authorities = new ArrayList<>();
 
@@ -48,6 +49,14 @@ public class UserServiceImpl implements UserService {
         return authorities;
     }
 
-   
+    @Override
+    public String login(User user) {
+        User returningUser = userRepository.findByUsername(user.getUsername());
+        if(returningUser != null && bCryptPasswordEncoder.matches(user.getPassword(), returningUser.getPassword())){
+            UserDetails userDetails = loadUserByUsername(returningUser.getUsername());
+            return jwtUtil.generateToken(userDetails);
+        }
+        return null;
+    }
 
 }
