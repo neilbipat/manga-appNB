df00f3e24f048535e6b296f7e70ff076039cccd7
Neil BipatE <neilbipat@gmail.com>
2019-11-06 16:07:46 -0500

    Added manga

---
+++ apimono/src/main/java/com/example/api/model/Manga.java
@@ -0,0 +1,93 @@
+package com.example.api.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "manga")
+public class Manga {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "manga_title")
+    private String manga_title;
+
+    @Column(name = "manga_chapter")
+    String manga_chapter;
+
+    @Column(name = "manga_page")
+    String manga_page;
+
+
+    public Manga() {}
+
+    /**
+     *
+     * @return id
+     */
+
+    public Long getId() {return id;}
+
+    /**
+     *
+     * @param id
+     */
+
+    public void setId(Long id) {this.id = id;}
+
+    /**
+     *
+     * @return manga_title
+     */
+
+    public String getManga_title() {return manga_title; }
+
+    /**
+     *
+     * @param manga_title
+     */
+
+    public void setManga_title(String manga_title) {this.manga_title = manga_title;}
+
+    /**
+     *
+     * @return manga_title
+     */
+
+    public String getManga_chapter() {return manga_chapter;}
+
+    /**
+     *
+     * @param manga_chapter
+     */
+
+    public void setManga_chapter(String manga_chapter) {this.manga_chapter = manga_chapter;}
+
+    /**
+     *
+     * @return manga_chapter
+     */
+
+    public String getManga_page() {return manga_page;}
+
+    /**
+     *
+     * @param manga_page
+     */
+
+    public void setManga_page(String manga_page) {this.manga_page = manga_page;}
+
+
+
+
+}
+
+}
