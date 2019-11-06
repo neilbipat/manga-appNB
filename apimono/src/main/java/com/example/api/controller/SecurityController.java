85d4853a8961038849ccf9ab1d4d13fbd569f0ca
Neil BipatE <neilbipat@gmail.com>
2019-11-06 16:25:14 -0500

    added Security Contoller

---
+++ apimono/src/main/java/com/example/api/controller/SecurityController.java
@@ -0,0 +1,19 @@
+package com.example.api.controller;
+
+import com.example.api.config.IAuthenticationFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityController {
+
+    @Autowired
+    private IAuthenticationFacade authenticationFacade;
+
+    public String getCurrentUserName() {
+        Authentication authentication = authenticationFacade.getAuthentication();
+        return authentication.getName();
+    }
+
+}
