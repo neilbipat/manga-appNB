037630cece9575d9c2686d36b5808d37dd5c10a5
Neil BipatE <neilbipat@gmail.com>
2019-11-06 15:54:53 -0500

    IAuthenticationFaacadeImpl

---
+++ apimono/src/main/java/com/example/api/config/IAuthenticationFacadeImpl.java
@@ -0,0 +1,15 @@
+package com.example.api.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IAuthenticationFacadeImpl implements IAuthenticationFacade {
+
+    @Override
+    public Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+}
