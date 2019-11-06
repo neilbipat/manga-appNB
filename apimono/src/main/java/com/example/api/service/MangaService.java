097608bc9f7abd41bb2c784e83f8c5bc68efdce9
Neil BipatE <neilbipat@gmail.com>
2019-11-06 16:20:10 -0500

    added MangaService

---
+++ apimono/src/main/java/com/example/api/service/MangaService.java
@@ -0,0 +1,27 @@
+package com.example.api.service;
+
+import com.example.api.model.Manga;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface MangaService {
+
+    /**
+     *
+     * @return
+     */
+
+    public List<Manga> listUserMangaTitle();
+
+    /**
+     *
+     * @return
+     */
+
+    public List<Manga> listUserMangaChapter();
+
+}
