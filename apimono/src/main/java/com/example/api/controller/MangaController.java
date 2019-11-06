29e0f62b887a1fe8fb53f483b6ed2ba56e6533ed
Neil BipatE <neilbipat@gmail.com>
2019-11-06 16:28:00 -0500

    Added apimono

---
+++ apimono/src/main/java/com/example/api/controller/MangaController.java
@@ -0,0 +1,25 @@
+package com.example.api.controller;
+
+import com.example.api.model.Manga;
+import com.example.api.service.MangaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/manga")
+public class MangaController {
+
+    @Autowired
+    MangaService mangaService;
+
+    @GetMapping("/listUserMangaTitle")
+    public List<Manga>   listUserMangaTitle() {return mangaService.listUserMangaTitle(); }
+
+    @GetMapping("/listUserMangaChapter")
+    public List<Manga> listUserMangaChapter() {return mangaService.listUserMangaChapter(); }
+
+}
