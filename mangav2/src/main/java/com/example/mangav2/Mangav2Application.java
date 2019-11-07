3f2759b50ad0a664c1abdc17d54509f811b5930c
Neil BipatE <neilbipat@gmail.com>
2019-11-07 18:04:16 -0500

    added mangav2 file

---
+++ mangav2/src/main/java/com/example/mangav2/Mangav2Application.java
@@ -0,0 +1,13 @@
+package com.example.mangav2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mangav2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mangav2Application.class, args);
+	}
+
+}
---
+++ mangav2/src/test/java/com/example/mangav2/Mangav2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mangav2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mangav2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
