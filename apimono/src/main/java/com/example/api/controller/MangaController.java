ab9dcc0c81fea595d54f16322c5818b684504dcb
Neil BipatE <neilbipat@gmail.com>
2019-11-07 10:50:48 -0500

    Added addManga

--- apimono/src/main/java/com/example/api/controller/MangaController.java
+++ apimono/src/main/java/com/example/api/controller/MangaController.java
@@ -4,9 +4,7 @@ import com.example.api.model.Manga;
 import com.example.api.repository.UserRepository;
 import com.example.api.service.MangaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/manga")
@@ -18,6 +16,9 @@ public class MangaController {
 
     @Autowired
     UserRepository userRepository;
+
+    @PostMapping("/add")
+    public Manga addManga(@RequestBody Manga newManga) {return mangaService.addManga(newManga);}
 
     @GetMapping("/listAllMangas")
     public Iterable<Manga> listAllMangas() {return mangaService.listAllMangaTitles();}
