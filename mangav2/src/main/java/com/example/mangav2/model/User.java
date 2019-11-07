dc2198e36f8d603564427e8789dfdbf5b04537a9
Neil BipatE <neilbipat@gmail.com>
2019-11-07 18:33:29 -0500

    added setUsername and getUsername to mangav2 modle user

--- mangav2/src/main/java/com/example/mangav2/model/User.java
+++ mangav2/src/main/java/com/example/mangav2/model/User.java
@@ -66,4 +66,12 @@ public class User {
         this.id = id;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
 }
