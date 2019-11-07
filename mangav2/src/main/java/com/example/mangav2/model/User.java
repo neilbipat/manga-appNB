81ffae56e46521ed4fc531b6241ef4e027334112
Neil BipatE <neilbipat@gmail.com>
2019-11-07 18:17:16 -0500

    added the columns to manva v2 model user

--- mangav2/src/main/java/com/example/mangav2/model/User.java
+++ mangav2/src/main/java/com/example/mangav2/model/User.java
@@ -7,5 +7,20 @@ import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import javax.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @Column
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
 }
