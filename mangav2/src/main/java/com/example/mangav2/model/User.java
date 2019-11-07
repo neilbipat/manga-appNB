05f328323c68a3a1ea279cf99db5fbd1e50b21b1
Neil BipatE <neilbipat@gmail.com>
2019-11-07 18:15:03 -0500

    Added imports to manga v2 model user

---
+++ mangav2/src/main/java/com/example/mangav2/model/User.java
@@ -0,0 +1,11 @@
+package com.example.mangav2.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class User {
+}
