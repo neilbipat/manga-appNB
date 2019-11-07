67f54907309545f5bb82124079572218d5414a61
Neil BipatE <neilbipat@gmail.com>
2019-11-07 18:44:01 -0500

    added imports to v2 modle manga

--- mangav2/src/main/java/com/example/mangav2/model/manga.java
+++ mangav2/src/main/java/com/example/mangav2/model/manga.java
@@ -1,4 +1,10 @@
 package com.example.mangav2.model;
 
+import javax.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import java.util.List;
+
 public class manga {
 }
