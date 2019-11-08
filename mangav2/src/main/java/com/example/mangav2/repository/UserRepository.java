e51919eda65d547161c076dd7e26d7f58516cbd8
Neil BipatE <neilbipat@gmail.com>
2019-11-07 19:09:18 -0500

    imported user model to user Repository

--- mangav2/src/main/java/com/example/mangav2/repository/UserRepository.java
+++ mangav2/src/main/java/com/example/mangav2/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.mangav2.repository;
 
+import com.example.mangav2.model.User;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
