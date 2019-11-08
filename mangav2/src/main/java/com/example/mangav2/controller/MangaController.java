b5d58cb83a4f9065c3c9392331812d67dd1ad32a
Neil BipatE <neilbipat@gmail.com>
2019-11-08 11:23:37 -0500

    added MangaController with imports

---
+++ mangav2/src/main/java/com/example/mangav2/controller/MangaController.java
@@ -0,0 +1,10 @@
+package com.example.mangav2.controller;
+
+import com.example.mangav2.model.Mangas;
+import com.example.mangav2.service.MangasService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MangaController {
+}
