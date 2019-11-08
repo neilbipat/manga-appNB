f04bf880c1e1f644f45b75b714fd71a7fe02c54e
Neil BipatE <neilbipat@gmail.com>
2019-11-07 19:17:13 -0500

    added mangasRepository with the imports

---
+++ mangav2/src/main/java/com/example/mangav2/repository/MangasRepository.java
@@ -0,0 +1,8 @@
+package com.example.mangav2.repository;
+
+import com.example.mangav2.model.Mangas;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface MangasRepository {
+}
