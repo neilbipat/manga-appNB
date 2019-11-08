961284d0c32a53a66fcf4c699bb65a2bd22e61a5
Neil BipatE <neilbipat@gmail.com>
2019-11-07 19:40:18 -0500

    completed IAuthenticationFacadeImpl

--- mangav2/src/main/java/com/example/mangav2/config/IAuthenticationFacadeImpl.java
+++ mangav2/src/main/java/com/example/mangav2/config/IAuthenticationFacadeImpl.java
@@ -4,5 +4,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
-public class IAuthenticationFacadeImpl {
+public class IAuthenticationFacadeImpl implements IAuthenticationFacade {
+
+    @Override
+    public Authentication getAuthentication() {return SecurityContextHolder.getContext().getAuthentication();}
 }
