1644749dfc06ddc3a9f100a963c6131ce22ef7df
Neil BipatE <neilbipat@gmail.com>
2019-11-07 19:46:55 -0500

    added securityConfig

---
+++ mangav2/src/main/java/com/example/mangav2/config/SecurityConfig.java
@@ -0,0 +1,77 @@
+package com.example.mangav2.config;
+
+import com.example.mangav2.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserService userService;
+
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter; // Authenticate whether the user exists or not.
+
+    @Bean("encoder")
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        //fixes cors method [needs bean - corsConfigurationSource()] and webconfig override method
+        http.cors();
+
+//        antMatchers() allows configuring the HttpSecurity to only be invoked when matching the provided ant pattern.
+//        csrf() stands for cross site forgery, which can steal user so we encrypt it
+//authentication states that anything inside of it such as user or profile if you put / then anything afterwards needs authentication
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/signup/**", "/login/**")
+                .permitAll()
+                .antMatchers("/user/**", "/mangas/**")
+                .authenticated()
+                .and()
+                .httpBasic();
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.unmodifiableList(Arrays.asList("*")));
+        configuration.setAllowedMethods(Collections.unmodifiableList(Arrays.asList("HEAD",
+                "GET", "POST", "PUT", "DELETE", "PATCH")));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Collections.unmodifiableList(Arrays.asList("Authorization", "Cache-Control", "Content-Type")));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+
+
+}
