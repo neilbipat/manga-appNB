67ca7dca0f7490f766fa67b790f14210eb2ee281
Neil BipatE <neilbipat@gmail.com>
2019-11-07 19:54:50 -0500

    added UserServiceImpl imports

--- mangav2/src/main/java/com/example/mangav2/service/UserServiceImpl.java
+++ mangav2/src/main/java/com/example/mangav2/service/UserServiceImpl.java
@@ -1,6 +1,24 @@
 package com.example.mangav2.service;
 
+import com.example.mangav2.config.JwtUtil;
+import com.example.mangav2.model.Mangas;
+import com.example.mangav2.model.User;
+import com.example.mangav2.repository.MangasRepository;
+import com.example.mangav2.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class UserServiceImpl {
+
+public class UserServiceImpl implements UserService {
 }
