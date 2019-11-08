51b72e5891cfdf706e4e17f63071969380ce08d0
Neil BipatE <neilbipat@gmail.com>
2019-11-07 19:50:39 -0500

    added JwtRequestFilter imports

---
+++ mangav2/src/main/java/com/example/mangav2/config/JwtRequestFilter.java
@@ -0,0 +1,20 @@
+package com.example.mangav2.config;
+
+import com.example.mangav2.config.JwtUtil;
+import com.example.mangav2.service.UserService;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import io.jsonwebtoken.ExpiredJwtException;
+
+
+public class JwtRequestFilter {
+}
