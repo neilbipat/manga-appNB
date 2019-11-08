917c339b35707801ca70b9acbc17a023bb6643f8
Neil BipatE <neilbipat@gmail.com>
2019-11-07 19:39:03 -0500

    added IAuthenticationFacadeImpl in config in mangav2

---
+++ mangav2/src/main/java/com/example/mangav2/config/IAuthenticationFacadeImpl.java
@@ -0,0 +1,8 @@
+package com.example.mangav2.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+public class IAuthenticationFacadeImpl {
+}
