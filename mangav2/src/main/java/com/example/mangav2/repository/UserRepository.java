f5008a2eecc945a1378a243bf7ee7e3d5cb0c989
Neil BipatE <neilbipat@gmail.com>
2019-11-07 19:13:24 -0500

    added login

--- mangav2/src/main/java/com/example/mangav2/repository/UserRepository.java
+++ mangav2/src/main/java/com/example/mangav2/repository/UserRepository.java
@@ -5,5 +5,9 @@ import com.example.mangav2.model.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public class UserRepository {
+public interface UserRepository extends CrudRepository<User, Long> {
+    @Query("From User u WHERE u.username = ?1 and u.password ?2")
+    public User login(String username, String password);
+
+    public User findByUsername(String username);
 }
