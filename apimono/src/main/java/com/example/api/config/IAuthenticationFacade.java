298445ce47719708ffb5fb3a14dfc66b04bf393f
Neil BipatE <neilbipat@gmail.com>
2019-11-08 13:34:01 -0500

    got rid of the other monos that dont work

--- apimono/src/main/java/com/example/api/config/IAuthenticationFacade.java
+++
@@ -1,9 +0,0 @@
-package com.example.api.config;
-
-import org.springframework.security.core.Authentication;
-
-public interface IAuthenticationFacade {
-
-    Authentication getAuthentication();
-
-}
