eb44735754d436161d09af647e70b616e4645fc0
Neil BipatE <neilbipat@gmail.com>
2019-11-07 20:24:24 -0500

    added MangaService with the imports and addMangaToDb and listMangas

--- mangav2/src/main/java/com/example/mangav2/service/MangaService.java
+++ mangav2/src/main/java/com/example/mangav2/service/MangaService.java
@@ -3,4 +3,11 @@ package com.example.mangav2.service;
 import com.example.mangav2.model.Mangas;
 
 public interface MangaService {
+
+    public Mangas addMangaToDB(Mangas newManga);
+
+    public Iterable<Mangas> listMangas();
+
+    
+
 }
