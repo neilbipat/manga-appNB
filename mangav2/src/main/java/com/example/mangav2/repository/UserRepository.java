7d6dfc5882b3b67bf81c35dc598c5be4e7e1d20e
Neil BipatE <neilbipat@gmail.com>
2019-11-07 19:08:07 -0500

    added UserRepository and imports

---
+++ mangav2/src/main/java/com/example/mangav2/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.mangav2.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public class UserRepository {
+}
