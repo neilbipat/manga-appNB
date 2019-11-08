821de62904483a14d7fbcba0e13ec5c4dd697c80
Neil BipatE <neilbipat@gmail.com>
2019-11-07 19:41:06 -0500

    added JwtUtil with imports

---
+++ mangav2/src/main/java/com/example/mangav2/config/JwtUtil.java
@@ -0,0 +1,18 @@
+package com.example.mangav2.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+
+public class JwtUtil {
+}
