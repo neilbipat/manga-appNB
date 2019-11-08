5780e430b7b638aad927024bb41bae601a7676d1
Neil BipatE <neilbipat@gmail.com>
2019-11-08 18:20:47 -0500

    Made a smart component

--- mangafront/src/App.js
+++ mangafront/src/App.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <input type = "text" placeholder="search"/>
-      
+class App extends Component{
+  render(){
 
+    return (
+      <div className="App">
+        <input type="text" placeholder="search" />
 
-    </div>
-  );
+      </div>
+    );
+
+  }
+ 
 }
 
 export default App;
