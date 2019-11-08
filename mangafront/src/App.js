3efd8c35cfa0b8cf11ff11965850c86ea0dcd306
Neil BipatE <neilbipat@gmail.com>
2019-11-08 18:18:32 -0500

    added a search bar

--- mangafront/src/App.js
+++ mangafront/src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <input type = "text" placeholder="search"/>
       
+
+
     </div>
   );
 }
