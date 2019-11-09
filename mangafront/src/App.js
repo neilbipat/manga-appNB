2defbd207a0f661415cb7faa15c566f7af1832ce
Neil BipatE <neilbipat@gmail.com>
2019-11-09 15:40:02 -0500

    added signup input bars in app js

--- mangafront/src/App.js
+++ mangafront/src/App.js
@@ -5,48 +5,31 @@ import './App.css';
 
 class App extends Component{
 
-  constructor(){
-    super();
-    this.state = {
-      apiLoaded: false
-    }
-  }
-  useInput = () => {
-    fetch(`https://www.mangaeden.com/api/chapter/5c56996c719a1689f3231217`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        console.log(res);
-        this.setState({
-          images: res.images[0]["d7/d72b5fb2022bb68e37065f9d2c2f003ec455a918c3d4d1cc209e7701.png"], 
-          apiLoaded: true
-        })  
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-  };
-
-  renderManga = ()  => {
-    return <p>{this.state.images}</p>
-  }
-
   render(){
-
-
     return (
       <div className="App">
-        <input type="text" placeholder="search" />
-        <button onClick={this.useInput}>Hey</button>
-        <div>
-          {this.state.apiLoaded ? this.renderManga(): "Your manga is loading"}
-        </div>
+        <form className="signupForm" method="post">
+          <div className="userNameDiv inputForm">
+            <input type = "text" className="userName signupName signupTexts"
+            placeholder="username"/>
+          </div>
+          <div className="emailDiv inputForm">
+            <input type ="text" className="email signupTexts"
+            placeholder="email"/>
+          </div>
+          <div className="passwordDiv inputForm">
+            <input type="text" className="password signupTexts"
+            placeholder="password"/>
+          </div>
+        </form>
+      
       </div>
     );
-
   }
- 
 }
 
 export default App;
+
+//  <Navbar></Navbar>
+// <Manga></Manga>
+// <Footer></Footer>
