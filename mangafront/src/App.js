249a2ff5529e723a7dc86e79533a2757be2f6bef
Neil BipatE <neilbipat@gmail.com>
2019-11-09 15:51:17 -0500

    added Signup

--- mangafront/src/App.js
+++ mangafront/src/App.js
@@ -8,20 +8,7 @@ class App extends Component{
   render(){
     return (
       <div className="App">
-        <form className="signupForm" method="post">
-          <div className="userNameDiv inputForm">
-            <input type = "text" className="userName signupName signupTexts"
-            placeholder="username"/>
-          </div>
-          <div className="emailDiv inputForm">
-            <input type ="text" className="email signupTexts"
-            placeholder="email"/>
-          </div>
-          <div className="passwordDiv inputForm">
-            <input type="text" className="password signupTexts"
-            placeholder="password"/>
-          </div>
-        </form>
+        
       
       </div>
     );
---
+++ mangafront/src/Nav.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function Nav(props){
+    return (
+        <div>
+            <form className="signupForm Nav" method="post">
+                <div className="userNameDiv inputForm">
+                    <input type="text" className="userName signupName signUpTexts"
+                        placeholder="username" />
+                </div>
+                <div className="emailDiv inputForm">
+                    <input type="text" className="email signUpEmail signUpTexts"
+                        placeholder="email" />
+                </div>
+                <div className="passwordDiv inputForm">
+                    <input type="text" className="password signUpPassword signUpTexts"
+                        placeholder="password" />
+                </div>
+                <button type="submit" class="button signUpButton" >Sign In</button>
+            </form>
+
+            <form className="logInForm Nav" method='post'>
+                <div className="userNameDiv inputForm">
+                    <input type="text" className="userName logInName logInTexts"
+                        placeholder="username" />
+                </div>
+                <div className="passwordDiv inputForm">
+                    <input type="text" className="password logInPassword logInTexts"
+                        placeholder="password" />
+                </div>
+                <button type="submit" class="button signUpButton">Log In</button>
+            </form>
+        </div>
+    )
+}
+
+export default Nav;
