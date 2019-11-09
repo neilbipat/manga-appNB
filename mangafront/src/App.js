ebda1ce38f7caa20b28f6dac2830e828fa4d3ba7
Neil BipatE <neilbipat@gmail.com>
2019-11-09 09:45:39 -0500

    added proper dependecies and mangav2 application

--- mangafront/src/App.js
+++ mangafront/src/App.js
@@ -2,12 +2,44 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component{
+
+  constructor(){
+    super();
+    this.state = {
+      apiLoaded: false
+    }
+  }
+  useInput = () => {
+    fetch(`https://www.mangaeden.com/api/list/0/`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        // console.log(res);
+        this.setState({
+          manga: res.manga[0].a, 
+          apiLoaded: true
+        })  
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  };
+
+  renderManga = ()  => {
+    return <p>{this.state.manga}</p>
+  }
+
   render(){
+
 
     return (
       <div className="App">
         <input type="text" placeholder="search" />
-
+        <button onClick={this.useInput}>Hey</button>
+        <div>
+          {this.state.apiLoaded ? this.renderManga(): "Your manga is loading"}
+        </div>
       </div>
     );
 
--- mangav2/src/main/java/com/example/mangav2/Mangav2Application.java
+++ mangav2/src/main/java/com/example/mangav2/Mangav2Application.java
@@ -2,9 +2,18 @@ package com.example.mangav2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient;
+@RestController;
 public class Mangav2Application {
+	@RequestMapping("/")
+	public String mangav2() {
+		return "some mangav2s";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Mangav2Application.class, args);
